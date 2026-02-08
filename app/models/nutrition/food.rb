9f96a906f081ec895b44c34e2f54598d25e649fa
soychicka <soychicka@gmail.com>
2026-02-07 20:21:34 -0600

    moving methods formerly on N:Food:Relations into Food model scopes

--- app/models/nutrition/food.rb
+++ app/models/nutrition/food.rb
@@ -3,5 +3,17 @@ module Nutrition
     belongs_to :food_group
     has_many :masses, :foreign_key => :nutrition_food_id
     has_and_belongs_to_many :langual_factors
+    
+    scope :fn, -> (str) {named(str).to_rel.db.order("length(name)") } do
+      # def method_name
+      #
+      # end
+    end
+    scope :named, -> (str) {
+      search( str,  
+      fields:["name^10",:description, :aliases, :origin], 
+      misspellings: { below: 5}
+      ).results#.to_rel.db#.order("length(name)")#,
+     }
   end
 end
